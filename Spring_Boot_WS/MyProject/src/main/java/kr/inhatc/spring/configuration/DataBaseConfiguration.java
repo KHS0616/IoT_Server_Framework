f9c6edaf830e2a497d77a64bbaa65bf408877bf8
KHS0616 <57310672+KHS0616@users.noreply.github.com>
2020-04-14 18:27:28 +0900

    Day 2020-04-14
    4월 14일
    스프링부트 기본설정
    DB연동

--- Spring_Boot_WS/MyProject/src/main/java/kr/inhatc/spring/configuration/DataBaseConfiguration.java
+++ Spring_Boot_WS/MyProject/src/main/java/kr/inhatc/spring/configuration/DataBaseConfiguration.java
@@ -1,11 +1,20 @@
 package kr.inhatc.spring.configuration;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -13,6 +22,9 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DataBaseConfiguration {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
 	
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource.hikari")
@@ -27,5 +39,20 @@ public class DataBaseConfiguration {
 		DataSource dataSource = new HikariDataSource(hikariConfig());
 		System.out.println("==========>" + dataSource.toString());
 		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception
+	{		
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory)
+	{
+		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 }
